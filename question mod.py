2adbb0b13c10506af540d45c11d98b6291c68150
kagwe <kagwepeter07@gmail.com>
2022-03-18 12:29:29 +0300

    'noUI'

--- question mod.py
+++ question mod.py
@@ -4,33 +4,46 @@ from chatterbot.trainers import ListTrainer
 import wave, math, contextlib
 import speech_recognition as sr
 from moviepy.editor import AudioFileClip
+import ast
 
 """ In this modeule we get the video transcript of the tutoial video"""
 # the saved file of the converted video
 
-transcribed_audio_file_name = "transcribed_speech.wav"
-zoom_video_file_name = "name_of_your_video.mp4"
+import speech_recognition as sr 
+import moviepy.editor as mp
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
-# video is first converted into audio
-audioclip = AudioFileClip(zoom_video_file_name)
-audioclip.write_audiofile(transcribed_audio_file_name)
+questions_text = []
 
-# divided into frames for ease of working with the video
-# the api also has a limit of file size request
-with contextlib.closing(wave.open(transcribed_audio_file_name,'r')) as f:
-    frames = f.getnframes()
-    rate = f.getframerate()
-    duration = frames / float(rate)
-total_duration = math.ceil(duration / 60)
-r = sr.Recognizer()
-#convert frames to txt
-for i in range(0, total_duration):
-    with sr.AudioFile(transcribed_audio_file_name) as source:
-        audio = r.record(source, offset=i*60, duration=60)
-    f = open("try.txt", "a")
-    f.write(r.recognize_google(audio))
-    f.write(" ")
-f.close()
+num_seconds_video= 52*60
+print("The video is {} seconds".format(num_seconds_video))
+l=list(range(0,num_seconds_video+1,60))
+
+diz={}
+for i in range(len(l)-1):
+    ffmpeg_extract_subclip("python.mp4", l[i]-2*(l[i]!=0), l[i+1], targetname="cut{}.mp4".format(i+1))
+    clip = mp.VideoFileClip(r"cut{}.mp4".format(i+1)) 
+    clip.audio.write_audiofile(r"converted{}.wav".format(i+1))
+    r = sr.Recognizer()
+    audio = sr.AudioFile("converted{}.wav".format(i+1))
+    with audio as source:
+      r.adjust_for_ambient_noise(source)  
+      audio_file = r.record(source)
+    result = r.recognize_google(audio_file)
+    diz['chunk{}'.format(i+1)]=result
+    l_chunks=[diz['chunk{}'.format(i+1)] for i in range(len(diz))]
+    text='\n'.join(l_chunks)
+    with open('tutorial_script.txt', 'r') as f:
+        n_data = f.readlines()
+        for element in l_chunks:
+            texts = element.strip('"')
+        n_data.append(texts)
+        n_data1 = []
+        for words in n_data:
+            n_data1.append(words.strip())
+    with open("tutorial_script.txt", "w") as f:
+        f.writelines('.\n'.join(n_data1))
+    
 
 
 # we create the questions from the video tutorial.
@@ -41,7 +54,7 @@ import nltk
 
 # file name - the genarated script from the tutorial
 # note - 'try.txt' is a dumy script
-scriptName = 'try.txt'
+scriptName = 'tutorial_script'
 
 #script opened
 def readFile(scriptName):
